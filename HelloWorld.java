94abaf470efb2c5745249d8ba827ce7b64bf1851
Sophia B. <118756221+smb78998@users.noreply.github.com>
2024-09-13 12:38:55 -0400

    add hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ class HelloWorld
 {
     public static void main(String []args)
     {
-        System.out.println("Hello Word?");
+        System.out.println("Hello Word :)");
+
+         printf("Hello, World!");
     }
 };
